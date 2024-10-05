aad67d0b776e1bc933afe1e02a2a4560b8e32e76
xaviertissera <78007807+xaviertissera@users.noreply.github.com>
2024-10-06 05:32:15 +1100

    update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // Import the Express framework for building the web server
-
 const express = require('express');
 const path = require('path'); // To handle file paths
 const app = express();
+
 // Import the API controller (concertAPI) from the 'controllerAPI/api-controller' file
 const concertAPI = require('./controllerAPI/api-controller');
 
@@ -10,8 +10,8 @@ const concertAPI = require('./controllerAPI/api-controller');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Middleware to serve static files from the public folder
-app.use(express.static('public'));
+// Middleware to serve static files from the public folder (including the admin folder)
+app.use(express.static(path.join(__dirname, 'public')));
 
 // All requests to '/api/concertsx' will be handled by the concertAPI module
 app.use("/api/concertsx", concertAPI);
@@ -21,8 +21,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server on port 3061
-const PORT = 3065;
+// Serve the admin panel index page
+app.get('/admin/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'admin', 'index.html'));
+});
+
+
+// Start the server on port 3066
+const PORT = 3066;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
